c895edf570df913d5ee80aaec265afa1d4c72607
Pronyo <franktb93@gmail.com>
2020-05-30 17:12:59 -0400

    Migrate server to typescript

--- web/server/src/server.ts
+++ web/server/src/server.ts
@@ -1,27 +1,28 @@
-const express = require('express');
-const app = express(),
-      bodyParser = require("body-parser");
-      port = 3080;
-const fs = require('fs-extra');
+import express from 'express';
+const app = express();
+import bodyParser from "body-parser";
+const PORT = process.env.PORT || 3000;
+import fs from 'fs-extra';
 const exec = require('child_process').exec;
 
-const multer  = require('multer')
+import multer from 'multer';
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, '..\\'),
+  destination: (req, file, cb) => cb(null, '..\\..\\'),
   filename: (req, file, cb) => cb(null, 'PM64.z64')
 })
 const upload = multer({storage: storage})
 
-const patchedRomPath = '..\\WorkingDir\\out\\PM64.z64';
-const cleanModPath = '..\\StarRod\ Mod\\'
-const workingModPath = '..\\WorkingDir\\'
-const cleanRomPath = '..\\PM64.z64'
+const patchedRomPath = '..\\..\\WorkingDir\\out\\PM64.z64';
+const cleanModPath = '..\\..\\StarRod\ Mod\\'
+const workingModPath = '..\\..\\WorkingDir\\'
+const cleanRomPath = '..\\..\\PM64.z64'
+var starRodJarPath = `${__dirname}\\..\\..\\..\\StarRod`; // For some reason only childProcess is picky and needs dirName
 
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.post('/api/patch', upload.single('inputRom', 1), (request, result) => {
+app.post('/api/patch', upload.single('inputRom', 1), (request: any, result) => {
   if(!isRomSizeValid(request.file)) {
     result.writeHead(400, "invalidRom");
     result.end('Invalid Rom');
@@ -30,15 +31,14 @@ app.post('/api/patch', upload.single('inputRom', 1), (request, result) => {
 
   fs.copy(cleanModPath, workingModPath)
   .then(() => {
-    console.log('running starRod')
-    const childProcess = exec('java -jar ..\\StarRod\\StarRod.jar -CompileMod', {cwd: '..\\StarRod'}, function(err, stdout, stderr) {
+    const childProcess = exec(`java -jar ${starRodJarPath}\\StarRod.jar -CompileMod`, {cwd: starRodJarPath}, function(err, stdout, stderr) {
       console.log(stdout);
       if (err) {
         console.log(err)
       }
     })
-    childProcess.on('exit', function() {
-      fs.exists(patchedRomPath)
+    childProcess.on('exit', () => {
+      fs.pathExists(patchedRomPath)
       .then((exists) => {
         if (exists) {     
           result.writeHead(200, {
@@ -52,7 +52,7 @@ app.post('/api/patch', upload.single('inputRom', 1), (request, result) => {
           outFile.pipe(result);
         } else {
           result.writeHead(400, {"Content-Type": "text/plain"});
-          result.end("ERROR File does not exist");
+          result.end("Failed to patch rom");
         }
       });
     })
@@ -63,10 +63,10 @@ app.post('/api/patch', upload.single('inputRom', 1), (request, result) => {
   
 });
 
-isRomSizeValid = (file) => file.size === 41943040;
+const isRomSizeValid = (file) => file.size === 41943040;
 
-app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
+app.listen(PORT, () => {
+    console.log(`Server listening on the port::${PORT}`);
 });
 
 
@@ -76,7 +76,6 @@ function replaceGombarioWithParakarry() {
       return console.log(err);
     }
     var result = data.replace('$Function_80241E90 ( 00000002 00000001 )', '$Function_80241E90 ( 00000004 00000004 )');
-    console.log(index)
     
     fs.writeFile('..\\StarRod\ Mod\\map\\patch\\kmr_02.mpat', result, 'utf8', function (err) {
        if (err) {
