00fee8cfa8dc15679880db3b773c4f28448afecc
Pronyo <franktb93@gmail.com>
2022-02-02 16:13:37 -0500

    Under Randomizer Settings > Items, disable all other switches if Shuffle Items is turned off

--- web/app/src/app/pages/home/randomizer-page/items/items.component.ts
+++ web/app/src/app/pages/home/randomizer-page/items/items.component.ts
@@ -1,17 +1,43 @@
 import { FormGroup } from '@angular/forms';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.scss']
 })
-export class ItemsComponent implements OnInit {
+export class ItemsComponent implements OnInit, OnDestroy {
+
+  private _shuffleItemsSubscription: any;
 
   @Input() public itemFormGroup: FormGroup;
-  constructor() { }
+  
+  public constructor() { }
+  
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    // Set every item setting to false if shuffleItems false
+    this._shuffleItemsSubscription = this.itemFormGroup.get("shuffleItems").valueChanges.pipe(
+      tap(() => {
+        if(this.itemFormGroup.get("shuffleItems").value == false){
+          Object.keys(this.itemFormGroup.controls).filter(formControl => formControl != "shuffleItems").forEach(formControl => {
+              this.itemFormGroup.get(formControl).setValue(false);
+              this.itemFormGroup.get(formControl).disable();
+          })
+        } else {
+          Object.keys(this.itemFormGroup.controls).filter(formControl => formControl != "shuffleItems").forEach(formControl => {
+            this.itemFormGroup.get(formControl).enable();
+        })
+        }
+      })
+    ).subscribe();
+  }
+
+  public ngOnDestroy(): void {
+    if(this._shuffleItemsSubscription) {
+      this._shuffleItemsSubscription.unsubscribe();
+    }
   }
 
 }
