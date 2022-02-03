6cea40a2ec3b42c91b15f720860a0c5646f0847b
Pronyo <franktb93@gmail.com>
2022-02-03 17:09:22 -0500

    Remove remains of old rando: server and mod

--- web/server/src/enums/equipUpgrade.ts
+++
@@ -1,7 +0,0 @@
-export enum EquipUpgrade {
-    BOOTS2 = 'Boots2',
-    BOOTS3 = 'Boots3',
-    HAMMER = 'Hammer',
-    HAMMER2 = 'Hammer2',
-    HAMMER3 = 'Hammer3'
-}
--- web/server/src/enums/itemLocationType.ts
+++
@@ -1,5 +0,0 @@
-export enum ItemLocationType {
-    KeyItem,
-    Partner,
-    EquipUpgrade
-}
