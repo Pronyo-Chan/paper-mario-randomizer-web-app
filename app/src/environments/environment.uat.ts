ac66e3b745cb1954a0189ad4ba2a335245521dbe
Pronyo <franktb93@gmail.com>
2022-02-07 09:21:16 -0500

    Setup UAT environment

---
+++ app/src/environments/environment.uat.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,  
+  apiEndPoint: "https://uat-dot-paper-mario-randomizer-server.ue.r.appspot.com/",
+  homepage:"https://uat-dot-paper-mario-randomizer-app.ue.r.appspot.com/",
+  currentModVersion: 1
+};
