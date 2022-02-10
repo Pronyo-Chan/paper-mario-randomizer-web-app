1100237c67df5ea6f1b6b9d865cf2ebbc3c88d0e
Pronyo <franktb93@gmail.com>
2022-02-09 19:44:49 -0500

    Turquoise help page + more tips

--- app/src/app/pages/help-page/help-page.component.ts
+++ app/src/app/pages/help-page/help-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, OnDestroy } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -6,14 +6,19 @@ import { environment } from 'src/environments/environment';
   templateUrl: './help-page.component.html',
   styleUrls: ['./help-page.component.scss']
 })
-export class HelpPageComponent implements OnInit {
+export class HelpPageComponent implements OnInit, OnDestroy {
   
   public homepageLink: string;
 
-  public constructor() { }
+  public constructor(private _renderer: Renderer2) { }
 
   public ngOnInit(): void {
     this.homepageLink = environment.homepage;
+    this._renderer.addClass(document.body, 'turquoise-bg');
+  }
+
+  public ngOnDestroy(): void {
+    this._renderer.removeClass(document.body, 'turquoise-bg')
   }
 
 }
