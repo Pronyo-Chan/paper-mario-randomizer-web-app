953738b50830f355ad4b04015e9089cff67b50ca
Pronyo <franktb93@gmail.com>
2022-02-18 16:02:51 -0500

    Improved status menu color palettes

--- app/src/app/utilities/constants.ts
+++ app/src/app/utilities/constants.ts
@@ -5,11 +5,12 @@ export class Constants {
     public static VALID_ROM_CRC: string = "2817903998"; //decimal value
     public static MENU_COLORS: MenuColor[] = [
         {colorA: 0xEBE677FF, colorB: 0x8E5A25FF}, // 0 = Default
-        {colorA: 0xFD744AFF, colorB: 0xB45031FF}, // 1 = Red
-        {colorA: 0x2748E1FF, colorB: 0x172F9FFF}, // 2 = Blue
-        {colorA: 0x22EEC9FF, colorB: 0x14A98EFF}, // 3 = Cyan
-        {colorA: 0x68FF51FF, colorB: 0x47B836FF}, // 4 = Green
-        {colorA: 0xAD72C2FF, colorB: 0x794E89FF}, // 5 = Purple
+        {colorA: 0x8D8FFFFF, colorB: 0x2B4566FF}, // 1 = Blue
+        {colorA: 0xAAD080FF, colorB: 0x477B53FF}, // 2 = Green
+        {colorA: 0x8ED4ECFF, colorB: 0x436245FF}, // 3 = Teal
+        {colorA: 0xD7BF74FF, colorB: 0x844632FF}, // 4 = Brown
+        {colorA: 0xB797B7FF, colorB: 0x62379AFF}, // 5 = Purple
+        {colorA: 0xC0C0C0FF, colorB: 0x404040FF}, // 6 = Grey
     ]
 
 }
