da86ba543e4988c3d44c93e52612d0699dd1374a
Pronyo <franktb93@gmail.com>
2022-02-24 17:44:03 -0500

    Enhance string functtion pascalToVerbose

--- app/src/app/utilities/stringFunctions.ts
+++ app/src/app/utilities/stringFunctions.ts
@@ -1,7 +1,18 @@
 export function pascalToVerboseString(text: string): string {
+    if(!text){
+        return null;
+    }
     var cleanText = text.replace(/([A-Z0-9])/g, " $1");
     cleanText = cleanText.charAt(0).toUpperCase() + cleanText.slice(1); 
-    cleanText = cleanText.replace("H P", "HP").replace("B P", "BP").replace("F P", "FP").replace("F X", "FX").replace("P O W", "POW").trimLeft()
+    cleanText = cleanText.replace("H P", "HP")
+        .replace("B P", "BP")
+        .replace("F P", "FP")
+        .replace("S P", "SP")
+        .replace("X P", "SP")
+        .replace("F X", "FX")
+        .replace("P O W", "POW")
+        .replace("O H K O", "One Hit KO")
+        .trimLeft()
 
     return cleanText;
 }
