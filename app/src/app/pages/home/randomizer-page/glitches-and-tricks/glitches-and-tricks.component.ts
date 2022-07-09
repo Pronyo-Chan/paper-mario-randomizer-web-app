80d4c2f20e2bce5b9f6f4547b37d2369a246fcf0
Pronyo <franktb93@gmail.com>
2022-07-09 13:03:51 -0400

    Add search filter and list headers

--- app/src/app/pages/home/randomizer-page/glitches-and-tricks/glitches-and-tricks.component.ts
+++ app/src/app/pages/home/randomizer-page/glitches-and-tricks/glitches-and-tricks.component.ts
@@ -10,15 +10,32 @@ import { LogicGlitch } from 'src/app/entities/logicGlitch';
 })
 export class GlitchesAndTricksComponent implements OnInit {
 
+  public readonly MIN_AMOUNT_OF_CHARS = 2;
+
   @Input() public formGroup: FormGroup
 
   public glitchesList: LogicGlitch[];
+  public filteredGlitches: LogicGlitch[];
+
+  public searchText: string;
 
   public constructor() { }
 
   public ngOnInit(): void {
-    this.glitchesList = glitchesJson
-    console.log(this.glitchesList)
+    this.glitchesList = glitchesJson;
+    this.filteredGlitches = this.glitchesList;
+  }
+
+  public filter() {
+    if(this.searchText.length < this.MIN_AMOUNT_OF_CHARS) {
+      this.filteredGlitches = this.glitchesList;
+      return;
+    }
+    this.filteredGlitches = this.glitchesList.filter(g => g.name.toLowerCase().includes(this.searchText.toLowerCase()))
+  }
+
+  public isGlitchInFilteredList(glitch: LogicGlitch) {
+    return this.filteredGlitches.some(g => g == glitch)
   }
 
 }
