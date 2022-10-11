9e90edae888a7c5e0666758fe13c0c925eebac39
Pronyo <franktb93@gmail.com>
2022-10-11 08:00:12 -0400

    0.16.0 release preparation

--- app/src/environments/environment.uat.ts
+++ app/src/environments/environment.uat.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,  
   apiEndPoint: "https://uat-dot-paper-mario-randomizer-server.ue.r.appspot.com/",
   homepage:"https://uat-dot-paper-mario-randomizer-app.ue.r.appspot.com/",
-  currentModVersion: 9
+  currentModVersion: 10
 };
