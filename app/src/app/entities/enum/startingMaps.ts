27c3824088d3332de879bcbc5ba669ab3f3a7a6a
Pronyo <franktb93@gmail.com>
2022-11-29 16:45:54 -0500

    Fix wrong comment in starting maps (random)

--- app/src/app/entities/enum/startingMaps.ts
+++ app/src/app/entities/enum/startingMaps.ts
@@ -3,5 +3,5 @@ export enum StartingMap {
     GoombaVillage = 0x00000101, // kmr_02 (1)
     DryDryOutpost = 0x00090100, // dro_02 (0)
     YoshiVillage = 0x00110302, // jan_03 (2)
-    Random = 0xFFFFFFFF, // jan_03 (2)
+    Random = 0xFFFFFFFF
 }
