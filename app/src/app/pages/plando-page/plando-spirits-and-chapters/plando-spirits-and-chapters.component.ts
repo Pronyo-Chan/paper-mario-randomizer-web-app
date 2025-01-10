a8cd1673b75cdfc1e2f52aa55eb4ce50c2063171
Veegie <jbalanuik@gmail.com>
2025-01-10 16:28:01 -0500

    Plando: Use stringFunctions instead of displayName for Spirit powers.

--- app/src/app/pages/plando-page/plando-spirits-and-chapters/plando-spirits-and-chapters.component.ts
+++ app/src/app/pages/plando-page/plando-spirits-and-chapters/plando-spirits-and-chapters.component.ts
@@ -1,14 +1,12 @@
 import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormGroup } from '@angular/forms';
+import { pascalToVerboseString } from "src/app/utilities/stringFunctions";
 
 type Boss = {
   id: string;
   displayName: string;
 }
-type SpiritPower = {
-  id: string;
-  displayName: string;
-}
+
 @Component({
   selector: 'app-plando-spirits-and-chapters',
   templateUrl: './plando-spirits-and-chapters.component.html',
@@ -17,15 +15,7 @@ type SpiritPower = {
 export class PlandoSpiritsAndChaptersComponent {
   @Input() plandoFormGroup: FormGroup;
 
-  public readonly SPRIT_POWERS: Array<SpiritPower> = [
-    {id: "Refresh", displayName: "Refresh"},
-    {id: "Lullaby", displayName: "Lullaby"},
-    {id: "StarStorm", displayName: "Star Storm"},
-    {id: "ChillOut", displayName: "Chill Out"},
-    {id: "Smooch", displayName: "Smooch"},
-    {id: "TimeOut", displayName: "Time Out"},
-    {id: "UpAndAway", displayName: "Up And Away"}
-  ];
+  public readonly SPRIT_POWERS: Array<string> = ['Refresh', 'Lullaby', 'StarStorm', 'ChillOut', 'Smooch', 'TimeOut', 'UpAndAway'];
   public readonly SPIRITS: Array<string> = ['Eldstar', 'Mamar', 'Skolar', 'Muskular', 'Misstar', 'Klevar', 'Kalmar'];
   public readonly BOSSES: Array<Boss> = [
     { id: '', displayName: '' },
@@ -37,6 +27,8 @@ export class PlandoSpiritsAndChaptersComponent {
     { id: 'HuffNPuff', displayName: 'Huff N Puff' },
     { id: 'CrystalKing', displayName: 'Crystal King' }];
   public requiredSpiritsArray = Array<string>();
+  
+  public toDisplayString = pascalToVerboseString;
 
   updateSpiritSelection($event: InputEvent, spirit: string) {
     if ($event.currentTarget instanceof HTMLInputElement) {
