4abb72c03d346ec3b4f3033739b0fc6d8ce6af68
Veegie <jbalanuik@gmail.com>
2025-01-10 15:45:17 -0500

    Plando: Party component cleanup. Drop un-needed displayName.

--- app/src/app/pages/plando-page/plando-party/plando-party.component.ts
+++ app/src/app/pages/plando-page/plando-party/plando-party.component.ts
@@ -1,15 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from "@angular/forms";
 import { MAX_FP_COST } from "../plando-page.component";
-
-type Move = {
-  id: string;
-  displayName: string;
-}
+import { pascalToVerboseString } from "src/app/utilities/stringFunctions";
 
 type Partner = {
   name: string;
-  moves: Array<Move>;
+  moves: Array<string>;
 }
 
 @Component({
@@ -21,60 +17,14 @@ export class PlandoPartyComponent {
   @Input() partnerMoveCostsFormGroup: FormGroup;
   public readonly MAX_FP: number = MAX_FP_COST;
   public readonly PARTNERS: Array<Partner> = [
-    {
-      name: 'Goombario',
-      moves: [
-        { id: 'Charge', displayName: 'Charge'},
-        { id: 'Multibonk', displayName: 'Multibonk'}]
-    },
-    {
-      name: 'Kooper',
-      moves: [
-        { id: 'PowerShell', displayName: 'Power Shell'},
-        { id: 'DizzyShell', displayName: 'Dizzy Shell'},
-        { id: 'FireShell', displayName: 'Fire Shell'}]
-    },
-    {
-      name: 'Bombette',
-      moves: [
-        { id: 'Bomb', displayName: 'Bomb'},
-        { id: 'PowerBomb', displayName: 'Power Bomb'},
-        { id: 'MegaBomb', displayName: 'Mega Bomb'}]
-    },
-    {
-      name: 'Parakarry',
-      moves: [
-        { id: 'ShellShot', displayName: 'Shell Shot'},
-        { id: 'AirLift', displayName: 'Air Lift'},
-        { id: 'AirRaid', displayName: 'Air Raid'}]
-    },
-    {
-      name: 'Bow',
-      moves: [
-        { id: 'OuttaSight', displayName: 'Outta Sight'},
-        { id: 'Spook', displayName: 'Spook'},
-        { id: 'FanSmack', displayName: 'Fan Smack'}]
-    },
-    {
-      name: 'Watt',
-      moves: [
-        { id: 'PowerShock', displayName: 'Power Shock'},
-        { id: 'TurboCharge', displayName: 'Turbo Charge'},
-        { id: 'MegaShock', displayName: 'Mega Shock'}]
-    },
-    {
-      name: 'Sushie',
-      moves: [
-        { id: 'Squirt', displayName: 'Squirt'},
-        { id: 'WaterBlock', displayName: 'Water Block'},
-        { id: 'TidalWave', displayName: 'Tidal Wave'}]
-    },
-    {
-      name: 'Lakilester',
-      moves: [
-        { id: 'SpinySurge', displayName: 'Spiny Surge'},
-        { id: 'CloudNine', displayName: 'Cloud Nine'},
-        { id: 'Hurricane', displayName: 'Hurricane'}]
-    }
+    { name: 'Goombario', moves: ['Charge', 'Multibonk'] },
+    { name: 'Kooper', moves: ['PowerShell', 'DizzyShell', 'FireShell'] },
+    { name: 'Bombette', moves: ['Bomb', 'PowerBomb', 'MegaBomb'] },
+    { name: 'Parakarry', moves: ['ShellShot', 'AirLift', 'AirRaid'] },
+    { name: 'Bow', moves: ['OuttaSight', 'Spook', 'FanSmack'] },
+    { name: 'Watt', moves: ['PowerShock', 'TurboCharge', 'MegaShock'] },
+    { name: 'Sushie', moves: ['Squirt', 'WaterBlock', 'TidalWave'] },
+    { name: 'Lakilester', moves: ['SpinySurge', 'CloudNine', 'Hurricane'] }
   ];
+  public toDisplayString = pascalToVerboseString;
 }
