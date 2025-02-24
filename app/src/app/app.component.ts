eb43906ad56b625a48cb034f2595bbbec579547d
Pronyo <franktb93@gmail.com>
2025-02-24 06:34:29 -0500

    Info modal for 0.29.0 update

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -16,8 +16,8 @@ export class AppComponent implements OnInit {
 
   public $latestSeedId: Observable<string>;
 
-  private readonly lastChangelogEntry = "viewed-0.28.0-changelog";
-  private readonly cutoffDate = new Date("2024-12-01");
+  private readonly lastChangelogEntry = "viewed-0.29.0-changelog";
+  private readonly cutoffDate = new Date("2025-03-10");
 
   public constructor(private _localStorage: LocalStorageService, private _dialog: MatDialog,) {
 
